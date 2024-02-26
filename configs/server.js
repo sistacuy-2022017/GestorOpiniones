c696d77df2aaf24253d10b9b982b70d26877f0ea
sistacuy-2022017 <sistacuy-2022017@kinal.edu.gt>
2024-02-26 11:01:02 -0600

    subiendo db

--- configs/server.js
+++ configs/server.js
@@ -4,12 +4,18 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
         this.middlewares();
+        this.conectarDB();
+    }
+
+    async conectarDB(){
+        await dbConnection();
     }
 
     middlewares() {
